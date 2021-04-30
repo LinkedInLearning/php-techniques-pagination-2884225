35df5328d4095ee3f0c486003cbda62a9f725006
Kevin Skoglund <kevin@novafabrica.com>
2021-04-30 17:01:24 -0400

    Finish movie 04_01

--- php_pagination/private/classes/pagination.class.php
+++ php_pagination/private/classes/pagination.class.php
@@ -2,6 +2,27 @@
 
 class Pagination {
 
+  public $current_page;
+  public $per_page;
+  public $total_count;
+
+  public function __construct($total_count=0, $page=1, $per_page=20) {
+    $this->per_page = (int) $per_page;
+    $this->total_count = (int) $total_count;
+    $this->current_page = (int) $page;
+    if($this->current_page < 1 || $this->current_page > $this->total_pages()) {
+      $this->current_page = 1;
+    }
+  }
+
+  public function offset() {
+    return $this->per_page * ($this->current_page - 1);
+  }
+
+  public function total_pages() {
+    return ceil($this->total_count / $this->per_page);
+  }
+
 }
 
 ?>
---
+++ php_pagination/public/customers_oo.php
@@ -0,0 +1,48 @@
+<?php
+
+include('../private/initialize.php');
+$db = db_connect();
+
+$total_count = count_customers();
+$page = $_GET['page'] ?? 1;
+$pagination = new Pagination($total_count, $page, 20);
+$customers = find_customers($pagination->per_page, $pagination->offset());
+
+?>
+
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Customer List</title>
+    <link rel="stylesheet" href="stylesheets/public.css">
+  </head>
+  <body>
+
+    <h1>Customer List</h1>
+
+    <p class="page-status">
+      Page <?php echo $pagination->current_page; ?> of <?php echo $pagination->total_pages(); ?>
+    </p>
+
+    <table id="customer-list">
+      <tr>
+        <th>First name</th>
+        <th>Last name</th>
+      </tr>
+
+      <?php
+        while($customer = db_fetch_assoc($customers)) {
+          echo "<tr>";
+          echo "<td>" . h($customer['first_name']) . "</td>";
+          echo "<td>" . h($customer['last_name']) . "</td>";
+          echo "</tr>";
+        } // end while
+        db_free_result($customers);
+      ?>
+    </table>
+
+  </body>
+</html>
+
+<?php if(isset($db)) { db_close($db); } ?>
